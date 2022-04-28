463e786e4b6d8bf9ff70c36a28803c23a1f8a064
Nivaldo Andrade <nivaldoandrade@MacBook-Air-de-Nivaldo.local>
2022-04-28 09:11:15 -0300

    first commit

---
+++ src/global/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+import theme from './styles/theme';
+
+
+declare module 'styled-components' {
+	type MyThemeType = typeof theme;
+
+	export interface DefaultTheme extends MyThemeType {}
+}
