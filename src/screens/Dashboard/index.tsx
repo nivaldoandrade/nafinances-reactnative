27b955e8d4571a149c05ca2463d24da064d9d9da
nivaldoandrade <nivaldoandradef@gmail.com>
2022-05-09 20:10:59 -0300

    create Header / add react-native-responsive-fontsize

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  HeaderWrapper,
+  User,
+  Photo,
+  UserInfo,
+  UserGratting,
+  UserName,
+  Icon
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
-      <Title>teste</Title>
+      <Header>
+        <SafeAreaView>
+          <HeaderWrapper>
+            <User>
+              <Photo
+                source={{
+                  uri: 'https://avatars.githubusercontent.com/u/66283205?v=4'
+                }}
+              />
+              <UserInfo>
+                <UserGratting>Olá,</UserGratting>
+                <UserName>Nivaldo</UserName>
+              </UserInfo>
+            </User>
+            <Icon name="power" />
+          </HeaderWrapper>
+        </SafeAreaView>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import {Feather} from '@expo/vector-icons';
 
 
 export const Container = styled.View`
 	flex: 1;
-	justify-content: center;
-	align-items: center;
 	background-color: ${({theme}) => theme.colors.background};
 `;
 
@@ -13,3 +13,54 @@ export const Title = styled.Text`
 	font-size: 24px;
 	color: ${({theme}) => theme.colors.title};
 `;
+
+export const Header = styled.View`
+	width: 100%;
+	height: ${RFPercentage(36)}px;
+	background-color: ${({theme}) => theme.colors.primary};
+
+	padding: 0 24px;
+`;
+
+export const HeaderWrapper = styled.View`
+	width: 100%;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+	padding-top: 28px;
+`;
+
+export const User = styled.View`
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+
+`;
+
+export const Photo = styled.Image`
+	width: ${RFValue(48)}px;
+	height: ${RFValue(48)}px;
+
+	border-radius: 10px;
+`;
+
+export const UserInfo = styled.View`
+	margin-left: 17px;
+`;
+
+export const UserGratting = styled.Text`
+	font-size: ${RFValue(18)}px;
+	font-family: ${({theme}) => theme.fonts.regular};
+	color: ${({theme}) => theme.colors.shape};
+`;
+
+export const UserName = styled.Text`
+	font-size: ${RFValue(18)}px;
+	font-family: ${({theme}) => theme.fonts.bold};
+	color: ${({theme}) => theme.colors.shape};
+`;
+
+export const Icon = styled(Feather)`
+	font-size: ${RFValue(24)}px;
+	color: ${({theme}) => theme.colors.secondary};
+` as unknown as typeof Feather;
