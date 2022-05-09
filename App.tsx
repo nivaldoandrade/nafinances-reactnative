ada99e60900b1aa7b6b61fb47fac2deeea576564
Nivaldo Andrade <nivaldoandrade@MacBook-Air-de-Nivaldo.local>
2022-05-08 22:05:49 -0300

    add: custom fonts

--- App.tsx
+++ App.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
 import { Dashboard } from './src/screens/Dashboard';
@@ -6,6 +13,14 @@ import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoader] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoader) return <AppLoading />;
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
